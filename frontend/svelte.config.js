6be94b63fcc77b870b33e6c2053c7e72984e7f57
Jeffrey Charters <jeff.charters@gmail.com>
2025-09-07 17:12:14 -0400

    sv preprocess

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,11 +1,11 @@
 import adapter from "@sveltejs/adapter-static"
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
+import { sveltePreprocess } from "svelte-preprocess"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: sveltePreprocess(),
 	kit: { adapter: adapter() }
 }
 
